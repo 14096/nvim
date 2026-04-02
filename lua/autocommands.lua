5a6c88745e1c3d8df7068ac19095675eb8a2a5b9
14096 <>
2026-04-02 09:28:49 +0200

    init

---
+++ lua/autocommands.lua
@@ -0,0 +1,52 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
+
+vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
+
+local restore_cursor_augroup = vim.api.nvim_create_augroup("restore_cursor_shape_on_exit", { clear = true })
+
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  group = restore_cursor_augroup,
+  desc = "restore the cursor shape on exit of neovim",
+  command = "set guicursor=a:hor20",
+})
+
+-- vim.api.nvim_create_autocmd("CursorHold", {
+-- callback = function()
+-- vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+-- end,
+-- })
---
+++ lua/utils/formatters.lua
@@ -0,0 +1,12 @@
+local formatters = {
+  "beautysh",
+  "clang-format",
+  "csharpier",
+  "gofumpt",
+  "jq",
+  "ormolu",
+  "prettier",
+  "stylua",
+}
+
+return formatters
